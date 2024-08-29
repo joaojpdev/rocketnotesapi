7a7a09ab97c72cd57fd56d938d5a2efde5a2d32b
joao-pedro <jpmmgs@gmail.com>
2024-08-29 10:22:53 -0300

    new commit

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -36,10 +36,16 @@ class NotesController {
     const {id} = req.params;
 
     const note = await knex("notes").where({id}).first()
+    const tags = await knex("tags").where({ note_id: id }).orderBy("name");
+    const links = await knex("links").where({ note_id: id }).orderBy("created_at");
 
-    return resp.json(note)
+    return resp.json({
+      ...note,
+      tags,
+      links
+    })
   }
-
+  
   async delete(req, resp) {
     const {id} = req.params;
 
