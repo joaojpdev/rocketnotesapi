f0ad565ece93f964b9a477e1e084da1be83cb3e3
joao-pedro <jpmmgs@gmail.com>
2024-09-23 11:26:24 -0300

    commit for deploy

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-require("express-async-errors")
+require("express-async-errors");
+require("dotenv/config");
 const migrationsRun = require("./database/sqlite/migrations");
 const uploadConfig = require("./configs/upload");
 
@@ -31,7 +32,7 @@ app.use(( error, request, response, next ) => {
   })
 })
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {console.log(`server is running on Port ${PORT}`)});
 
